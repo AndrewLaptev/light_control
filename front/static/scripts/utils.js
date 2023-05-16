bfd492c04e7413cec26afb6c573d3d1b6c8caf42
Andrew Laptev <nickname.avast@gmail.com>
2023-05-16 20:14:02 +0300

    fix: refactor errors

--- front/static/scripts/utils.js
+++ front/static/scripts/utils.js
@@ -3,7 +3,7 @@ export const COOKIE_NAME_LAMP_NUMBER = "lamp_number"
 export const COOKIE_NAME_COLOR_TEMP = "temperature"
 export const COOKIE_NAME_COLOR_BRIGHT = "brightness"
 
-export const LINK_ROOT = "/"
+export const LINK_ROOT = "/light-control/"
 export const LINK_LAMP_DATA = LINK_ROOT + "light/lamp-data"
 
 
@@ -14,14 +14,14 @@ export function getCookie(name) {
 }
 
 export function deleteCookies() {
-    var Cookies = document.cookie.split(';');
-    for (var i = 0; i < Cookies.length; i++) {
-        document.cookie = Cookies[i] + "=;expires=" + new Date(0).toUTCString();
+    const cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        document.cookie = cookies[i] + "=;expires=" + new Date(0).toUTCString();
     }
 }
 
 export async function errorInfo(response) {
-    error = await response.json()
+    const error = await response.json()
     if (typeof error["detail"] == "object") {
         return "Something went wrong!"
     } else {
